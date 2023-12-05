bf638d51aaf76e5094fdacbada9aab2ad8cb5c20
elzpdlem49 <elzpdlem49@gmail.com>
2023-12-05 17:04:08 +0900

    커밋

---
+++ KCPP/Assets/Script/Gun.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float shotPower = 500f;
+    public GameObject prefabBullet;
+    
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shot();
+        }
+    }
+    public void Shot()
+    {
+        Debug.Log("Shot");
+        GameObject Bullet = Instantiate(prefabBullet);
+        Bullet.transform.position = this.gameObject.transform.position;
+        Bullet.GetComponent<Rigidbody>().AddForce(transform.forward * shotPower);
+    }
+}
