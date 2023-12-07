875bbd4df2a0098bc51d4bd2de3ea0ea2237b4fd
elzpdlem49 <150213875+elzpdlem49@users.noreply.github.com>
2023-12-08 00:01:01 +0900

    코루틴 수정 중

--- KCPP/Assets/Script/BossCont.cs
+++ KCPP/Assets/Script/BossCont.cs
@@ -6,29 +6,44 @@ using TextRPG;
 
 public class BossCont : MonoBehaviour
 {
+    public static BossCont Instance;
+    [SerializeField]
+    public Player m_Boss;
     public Transform trfPlayer;
-    public TextRPG.Status m_Boss;
+    //public TextRPG.Status m_Boss;
     public float moveSpeed = 10f;
     public float serchRange = 20f;
     public float attackRange = 3f;
 
 
     Rigidbody bossRb;
-    public float attackCooldown = 5f;
+    public float attackCooldown = 10000f;
     bool isAttackCooldown = false;
-    public int m_BossDamage1 = 10;
-    public int m_BossDamage2 = 20;
-    public int m_BossDamage3 = 30;
+    public int m_BossDamage1 = 1;
+    public int m_BossDamage2 = 2;
+    public int m_BossDamage3 = 3;
 
     public bool isTouch;
     // Start is called before the first frame update
     void Start()
     {
         bossRb = GetComponent<Rigidbody>();
-        m_Boss = new Status(1000, 1000, 10, 10);
+        m_Boss = new Player(name, 1000, 1000, 10, 9);
+        Instance = this;
         //StartCoroutine(BossFight());
     }
-
+    public void OnGUI()
+    {
+        //오브젝트의 3d좌표를 2d좌표(스크린좌표)로 변환하여 GUI를 그린다.
+        Vector3 vPos = this.transform.position;
+        Vector3 vPosToScreen = Camera.main.WorldToScreenPoint(vPos); //월드좌표를 스크린좌표로 변환한다.
+        vPosToScreen.y = Screen.height - vPosToScreen.y; //y좌표의 축이 하단을 기준으로 정렬되므로 상단으로 변환한다.
+        int h = 40;
+        int w = 100;
+        Rect rectGUI = new Rect(vPosToScreen.x, vPosToScreen.y, w, h);
+        //GUI.Box(rectGUI, "MoveBlock:" + isMoveBlock);
+        GUI.Box(rectGUI, string.Format("HP:{1}\nMP:{0}", m_Boss.m_nMp, m_Boss.m_nHp));
+    }
     // Update is called once per frame
     private void Update()
     {
@@ -52,10 +67,10 @@ public class BossCont : MonoBehaviour
             if (Vector3.Distance(transform.position, trfPlayer.position) < attackRange)
             {
                 bossRb.velocity = Vector3.zero;
-
-               /* Quaternion toRotation = Quaternion.LookRotation(direction, Vector3.up);
-                toRotation = Quaternion.Lerp(transform.rotation, toRotation, Time.deltaTime * 5f);
-                enemyRigidbody.MoveRotation(toRotation);*/
+                // 바라보는 방향
+                /* Quaternion toRotation = Quaternion.LookRotation(direction, Vector3.up); 
+                 toRotation = Quaternion.Lerp(transform.rotation, toRotation, Time.deltaTime * 5f);
+                 enemyRigidbody.MoveRotation(toRotation);*/
 
                 StartCoroutine(AttackPattern());
             }
@@ -77,7 +92,7 @@ public class BossCont : MonoBehaviour
         }
     }*/
 
-    IEnumerator AttackPattern()
+    /*IEnumerator AttackPattern()
     {
         isAttackCooldown = true;
         Debug.Log("Boss is attacking!");
@@ -97,28 +112,70 @@ public class BossCont : MonoBehaviour
         }
 
         yield return new WaitForSeconds(attackCooldown);
+        isAttackCooldown = false;
     }
     IEnumerator AttackPattern1()
     {
         Debug.Log("Executing Attack Pattern 1");
-        //m_cPlayer.m_nHp -= m_BossDamage1;
-        yield return null;
+        PlayerMove.Instance.m_cPlayer.m_nHp -= m_Boss.m_sStatus.nStr * m_BossDamage1;
+        yield return new WaitForSeconds(attackCooldown); ;
     }
 
     IEnumerator AttackPattern2()
     {
         Debug.Log("Executing Attack Pattern 2");
-        //m_cPlayer.m_nHp -= m_BossDamage2;
-        yield return null;
+        PlayerMove.Instance.m_cPlayer.m_nHp -= m_Boss.m_sStatus.nStr * m_BossDamage2;
+        yield return new WaitForSeconds(attackCooldown); ;
     }
 
     IEnumerator AttackPattern3()
     {
         Debug.Log("Executing Attack Pattern 3");
-        //m_cPlayer.m_nHp -= m_BossDamage3;
-        yield return null;
+        PlayerMove.Instance.m_cPlayer.m_nHp -= m_Boss.m_sStatus.nStr * m_BossDamage3;
+        yield return new WaitForSeconds(attackCooldown); ;
+    }*/
+
+    IEnumerator AttackPattern()
+    {
+        isAttackCooldown = true;
+        Debug.Log("Boss is attacking!");
+        int random = Random.Range(0, 3);
+
+        switch (random)
+        {
+            case 0:
+                AttackPattern1();
+                break;
+            case 1:
+                AttackPattern2();
+                break;
+            case 2:
+                AttackPattern3();
+                break;
+        }
+        
+        // Introduce an additional delay specific to the attack patterns
+        yield return new WaitForSecondsRealtime(attackCooldown);
+
+        isAttackCooldown = false; // Reset isAttackCooldown after the delay
+    }
+    void AttackPattern1()
+    {
+        Debug.Log("Executing Attack Pattern 1");
+        PlayerMove.Instance.m_cPlayer.m_nHp -= m_Boss.m_sStatus.nStr * m_BossDamage1;
     }
 
+    void AttackPattern2()
+    {
+        Debug.Log("Executing Attack Pattern 2");
+        PlayerMove.Instance.m_cPlayer.m_nHp -= m_Boss.m_sStatus.nStr * m_BossDamage2;
+    }
+
+    void AttackPattern3()
+    {
+        Debug.Log("Executing Attack Pattern 3");
+        PlayerMove.Instance.m_cPlayer.m_nHp -= m_Boss.m_sStatus.nStr * m_BossDamage3;
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == ("Player"))
