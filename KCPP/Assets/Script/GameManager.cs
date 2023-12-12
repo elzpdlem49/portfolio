a1b0c741a0debccf369403498c170d519b24a902
elzpdlem49 <elzpdlem49@gmail.com>
2023-12-12 14:05:43 +0900

    윈도우 빌드

--- KCPP/Assets/Script/GameManager.cs
+++ KCPP/Assets/Script/GameManager.cs
@@ -45,12 +45,6 @@ public class GameManager : MonoBehaviour
     {
         SetGUIScene((E_GUI_STATE)stateNumber);
     }
-    /* public void EventUpdateStatus(int playerIdx = 0)
-     {
-         Player player = m_listPlayer[playerIdx].m_cPlayer;
-         if (player != null)
-             m_guiHPBar.SetBar(player.m_nHp, player.m_sStatus.nHP);
-     }*/
     public void ShowScenec(E_GUI_STATE state)
     {
         for (int i = 0; i < m_listGUIScenes.Count; i++)
