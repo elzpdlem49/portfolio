82b8b1465f3b9dae3ee97518c23672972125b00b
elzpdlem49 <150213875+elzpdlem49@users.noreply.github.com>
2023-12-18 21:24:55 +0900

    걷기 달리기

--- KCPP/Assets/Script/PlayerMove.cs
+++ KCPP/Assets/Script/PlayerMove.cs
@@ -10,8 +10,10 @@ public class PlayerMove : MonoBehaviour
     Rigidbody m_Rigdbody;
     Vector3 m_Walk;
     Vector3 m_Run;
+    Vector3 m_Sprint;
     public float m_speed = 1f;
-    public float m_RunSpeed = 4f;
+    public float m_RunSpeed = 3f;
+    public float m_SprintSpeed = 7f;
     public float m_JumpForce = 5f;
     public float m_dashForce = 15f;
     public float m_DashCoolTime = 0.5f;
@@ -19,8 +21,11 @@ public class PlayerMove : MonoBehaviour
     
     public bool isWalking = false;
     public bool isRuning = true;
-    public bool isRunorWalk;
+    public bool isRunorWalk = true;
+    public bool isSprinting = false;
     public bool isDashing = false;
+    public bool isJumping = false;
+    public bool isDodging = false;
     public bool isGrounded;
     //public bool isRolling;
     float hz;
@@ -41,28 +46,31 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButtonDown("Jump")  && isSprinting)
         {
-            Jump();
+            isJumping = true;
         }
-
-        if (Input.GetButtonDown("Jump") && !isDashing)
+        if (Input.GetKey(KeyCode.Space) && (isWalking || isRuning))
+        {
+            StartCoroutine(CorSprint());
+        }
+        /*if (Input.GetButtonDown("Jump") && !isDashing)
         {
             StartCoroutine(Dash());
-        }
-        if (Input.GetButtonDown("Fire1"))
+        }*/
+        if (Input.GetKeyDown(KeyCode.LeftControl))
         {
             ToggleRun();
         }
 
-        anim.SetBool("Walk", isWalking);
-        anim.SetBool("Run", isRuning);
-        
+        anim.SetBool("isWalk", isWalking);
+        anim.SetBool("isRun", isRuning);
+        anim.SetBool("RunatWalk", isRunorWalk);
+        anim.SetBool("isSprint", isSprinting);
     }
     void ToggleRun()
     {
         isRunorWalk = !isRunorWalk;
-        
     }
     
     private void FixedUpdate()
@@ -80,6 +88,7 @@ public class PlayerMove : MonoBehaviour
 
         m_Walk = moveDirection * m_speed * Time.deltaTime;
         m_Run = moveDirection * m_RunSpeed * Time.deltaTime;
+        m_Sprint = moveDirection * m_SprintSpeed * Time.deltaTime;
 
         if (m_Walk.magnitude > 0.01f)
         {
@@ -107,12 +116,22 @@ public class PlayerMove : MonoBehaviour
                 isWalking = false;
             }
         }
-        if (isRuning)
-        {
+       if (isRuning)
             Runing();
+        else if (isWalking)
+            Walk();
+        if ((isRuning || isWalking) && isSprinting)
+        {
+            Sprint();
         }
-        else
-            m_Rigdbody.MovePosition(transform.position + m_Walk); // 플레이어 이동 
+    }
+    void Sprint()
+    {
+        m_Rigdbody.MovePosition(transform.position + m_Sprint);
+    }
+    void Walk()
+    {
+        m_Rigdbody.MovePosition(transform.position + m_Walk); // 플레이어 이동 
     }
     void Runing()
     {
@@ -126,13 +145,22 @@ public class PlayerMove : MonoBehaviour
     {
         isGrounded = false;
     }
-   
-    void Jump()
-    {
-        m_Rigdbody.AddForce(Vector3.up * m_JumpForce, ForceMode.Impulse);
-    }
 
-    IEnumerator Dash()
+    IEnumerator Jump()
+    {
+        isJumping = true;
+        //m_Rigdbody.AddForce(Vector3.up * m_JumpForce, ForceMode.Impulse);
+        yield return new WaitForSeconds(0.1f);
+        isJumping = false;
+    }
+    IEnumerator CorSprint()
+    {
+        isSprinting = true;
+        yield return new WaitForSeconds(1f);
+        isSprinting = false;
+        
+    }
+   /* IEnumerator Dash()
     {
         isDashing = true;
         m_speed = m_dashForce;
@@ -143,5 +171,5 @@ public class PlayerMove : MonoBehaviour
 
         m_speed = m_OriginalSpeed;
         isDashing = false;
-    }  
+    }  */
 }  
